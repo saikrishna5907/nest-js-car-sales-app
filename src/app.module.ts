da7d5b58f05bb63daffaffaefa3e40a47251d3ac
Saikrishna <saikrishna.sangishetty@nextlogistics.com.au>
2022-05-04 15:20:54 +0100

    test db added for e2e

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,24 +5,34 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 const cookieSession = require('cookie-session');
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true
+        }
+      }
     }),
     UsersModule,
-    ReportsModule
+    ReportsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    })
   ],
   controllers: [],
 
   providers: [
     // here whitelist will validate if the request body is exactly expecting same properties as expected,
     // throws error if client sends extra properties or different properties 
-    { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) }
+    { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
   ],
 })
 export class AppModule {
